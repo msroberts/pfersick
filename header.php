4327d26be53cf603cfb7796fc7b5616b6999d2f7
msroberts <msroberts@users.noreply.github.com>
2016-08-25 20:53:25 -0400

    Add svg and basic styling to menu toggle button

--- header.php
+++ header.php
@@ -42,7 +42,14 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pfersick' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<svg fill="#FFFFFF" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
+					<path d="M0 0h24v24H0z" fill="none"/>
+					<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z">
+						<title><?php esc_html_e( 'Primary Menu', 'pfersick' ); ?><title>
+					</path>
+				</svg>
+			</button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
